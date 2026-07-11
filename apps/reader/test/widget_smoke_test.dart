19880bf407c13311cec404c6586ad0fcbfb6b701
thekester <theophile.avenel@gmail.com>
2026-07-11 16:40:11 +0200

    Stabilize reader widget smoke test

--- apps/reader/test/widget_smoke_test.dart
+++ apps/reader/test/widget_smoke_test.dart
@@ -1,8 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 import 'package:synkfeed_reader/app/synkfeed_app.dart';
 
 void main() {
   testWidgets('renders the SynkFeed shell', (tester) async {
+    addTearDown(() async {
+      await tester.binding.setSurfaceSize(null);
+    });
+
+    await tester.binding.setSurfaceSize(const Size(1400, 1000));
     await tester.pumpWidget(const SynkFeedApp());
     await tester.pumpAndSettle();
 
